1ca51197c2bd31b14abcf6c707834c55096ba433
Fangyuan Wang, 王方圆 <wangfangyuan1995@gmail.com>
2017-10-06 10:11:34 -0400

    added route for quotaReached

--- src/main/java/edu/rosehulman/billing/router/Routes.java
+++ src/main/java/edu/rosehulman/billing/router/Routes.java
@@ -2,6 +2,7 @@ package edu.rosehulman.billing.router;
 
 public class Routes {
 	public static String ADD_USER = "partner/:partnerId/product/:productId/addUser/:userId";
-
+	public static String QUOTA_REACHED = "/partner/:partnerId/product/:productId/user/:userId/quotaReached/:quotaId/";
+	
 	public static String BILLING_BASE = "/"; // TODO put this in a config file
 }
