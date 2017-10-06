8e76411895e5d6ac0a1142152c6661a78f89af07
unknown <dingy2@rose-hulman.edu>
2017-10-06 11:26:40 -0400

    test

--- src/main/java/edu/rosehulman/billing/BillingClient.java
+++ src/main/java/edu/rosehulman/billing/BillingClient.java
@@ -18,26 +18,31 @@ public class BillingClient {
 	static int pathmode = 1;
 
 	public static void main(String[] args) {
-		while (true) {
-			System.out.println("Socket:");
-			int socketNumber = 5697;
-
-			Socket socket = null;
-			String host = "35.164.184.155";
-			try {
-				socket = new Socket(host, socketNumber);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			// DO STUFF HERE.
-			try {
-				socket.close();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		System.out.println("Socket:");
+		BufferedReader socketNumberReader = new BufferedReader(new InputStreamReader(System.in));
+		String socketNumberString = null;
+		try {
+			socketNumberString = socketNumberReader.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+		int socketNumber = Integer.parseInt(socketNumberString);
 
+		Socket socket = null;
+		String host = "localhost";
+		try {
+			socket = new Socket(host, socketNumber);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		// DO STUFF HERE.
+		System.out.println("!!!");
+		try {
+			socket.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	// }
