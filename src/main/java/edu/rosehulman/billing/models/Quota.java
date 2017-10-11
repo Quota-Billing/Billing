03bfa0f59712f129c47d64e1b46cf5f38c97c1cf
Fangyuan Wang, 王方圆 <wangfangyuan1995@gmail.com>
2017-10-10 22:50:07 -0400

    added getter and setter for quota and tier object

--- src/main/java/edu/rosehulman/billing/models/Quota.java
+++ src/main/java/edu/rosehulman/billing/models/Quota.java
@@ -7,32 +7,40 @@ public class Quota {
 	private String name;
 	private String type;
 	private List<Tier> tiers;
-	
-	public Quota(int id, String name, String type){
+
+	public Quota(int id, String name, String type) {
 		this.id = id;
 		this.name = name;
-		this.type=type;
+		this.type = type;
 	}
-	
+
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
-		builder.append("Quota: "  + id + "\n");
-		for(Tier t: tiers) {
+		builder.append("Quota: " + id + "\n");
+		for (Tier t : tiers) {
 			builder.append(t.toString());
 		}
 		return builder.toString();
 	}
-	
-	public int getId(){
+
+	public int getId() {
 		return this.id;
 	}
-	
-	public String getName(){
+
+	public String getName() {
 		return this.name;
 	}
-	
-	public String getType(){
+
+	public String getType() {
 		return this.type;
+	}
+
+	public List<Tier> getTiers() {
+		return tiers;
+	}
+
+	public void setTiers(List<Tier> tiers) {
+		this.tiers = tiers;
 	}
 }
