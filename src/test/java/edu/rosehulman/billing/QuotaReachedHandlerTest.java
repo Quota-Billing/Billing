4f4efe0f42275345a5eefd9d594ccba8ff3ee69c
Fangyuan Wang, 王方圆 <wangfangyuan1995@gmail.com>
2017-10-17 20:20:44 -0400

    added dependency for running test

---
+++ src/test/java/edu/rosehulman/billing/QuotaReachedHandlerTest.java
@@ -0,0 +1,14 @@
+package edu.rosehulman.billing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class QuotaReachedHandlerTest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
