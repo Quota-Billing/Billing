e4fce9071deeb239e095dfed9b1756a07c104be2
Fangyuan Wang, 王方圆 <wangfangyuan1995@gmail.com>
2017-10-24 13:43:25 -0400

    refactor tests for data models

--- src/main/java/edu/rosehulman/billing/models/Partner.java
+++ src/main/java/edu/rosehulman/billing/models/Partner.java
@@ -5,18 +5,45 @@ import java.util.HashMap;
 public class Partner {
 	private int id;
 	private String name;
-	private HashMap<Integer, Product> products;
+	private HashMap<Integer, Product> products = new HashMap<Integer, Product>();
+	
+	public Partner(){
+		
+	}
 
 	public Partner(int partnerId, String name) {
 		this.id = partnerId;
 		this.name = name;
-		this.products = new HashMap<Integer, Product>();
 	}
 
 	public void addProduct(Product product) {
 		this.products.put(product.getId(), product);
 	}
 
+	public Product getProduct(int productId) {
+		return this.products.get(productId);
+	}
+	
+	public void removeProduct(Product product) {
+		this.products.remove(product.getId());
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
@@ -25,22 +52,5 @@ public class Partner {
 			builder.append(this.products.get(id).toString());
 		}
 		return builder.toString();
-
-	}
-
-	public Product getProduct(int productId) {
-		return this.products.get(productId);
-	}
-
-	public int getId() {
-		return this.id;
-	}
-
-	public String getName() {
-		return this.name;
-	}
-
-	public void removeProduct(Product product) {
-		this.products.remove(product.getId());
 	}
 }
--- src/main/java/edu/rosehulman/billing/models/Product.java
+++ src/main/java/edu/rosehulman/billing/models/Product.java
@@ -5,38 +5,59 @@ import java.util.HashMap;
 public class Product {
 	private int id;
 	private String name;
-	private HashMap<Integer, Quota> quotas;
+	private HashMap<Integer, Quota> quotas = new HashMap<Integer, Quota>();
+	
+	public Product() {
+		
+	}
 
 	public Product(int id, String name) {
 		this.id = id;
 		this.name = name;
-		quotas = new HashMap<Integer, Quota>();
 	}
 
-	public void addQuota(Quota quota) {
-		this.quotas.put(quota.getId(), quota);
+	public void setId(int id) {
+		this.id = id;
 	}
-
-	@Override
-	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("Product: " + id + "\n");
-		for (Integer id : this.quotas.keySet()) {
-			builder.append(this.quotas.get(id).toString());
-		}
-		return builder.toString();
-	}
-
+	
 	public int getId() {
 		return this.id;
 	}
 
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 	public String getName() {
 		return this.name;
 	}
 	
+	public Quota getQuota(int quotaId) {
+		return this.quotas.get(quotaId);
+	}
+	
+	public HashMap<Integer, Quota> getQuotas() {
+		return quotas;
+	}
+	
 	public void removeQuota(Quota q) {
 		this.quotas.remove(q.getId());
+	}
+	
+	public void addQuota(Quota quota) {
+		this.quotas.put(quota.getId(), quota);
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Product: " + id + "\n");
+		builder.append("Name: "+name + "\n");
+		builder.append("Quotas: ");
+		for (Integer id : this.quotas.keySet()) {
+			builder.append(this.quotas.get(id).toString());
+		}
+		return builder.toString();
 	}
 
 }
