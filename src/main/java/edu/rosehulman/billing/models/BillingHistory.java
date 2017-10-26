bea6240a0ffc26f884032815a88f5e8a6f3c59e8
unknown <dingy2@rose-hulman.edu>
2017-10-26 18:36:10 -0400

    Add Billing history billing model

---
+++ src/main/java/edu/rosehulman/billing/models/BillingHistory.java
@@ -0,0 +1,43 @@
+package edu.rosehulman.billing.models;
+
+import java.util.ArrayList;
+
+public class BillingHistory {
+
+	private int billinghistoryId;
+	private ArrayList<Billing> billing;
+	private ArrayList<Integer> billingId;
+	private ArrayList<String> time_stamp;
+	private ArrayList<Double> fee;
+	
+
+	public BillingHistory(int id) {
+		this.billinghistoryId = id;
+		billingId = new ArrayList<Integer>();
+		time_stamp = new ArrayList<String>();
+		fee = new ArrayList<Double>();
+	}
+
+	public int getHistoryId() {
+		return this.billinghistoryId;
+	}
+
+	public void addBilling(int billingid) {
+		this.billingId.add(billingid);
+	}
+
+	public double getFee(int billingid) {
+		return this.fee.get(this.billingId.indexOf(billingid));
+	}
+
+	public String getTimeStamp(int billingid) {
+		return this.time_stamp.get(this.billingId.indexOf(billingid));
+	}
+	
+	public void addBilling(Billing bl, String timestamp, double fee) {
+		this.billing.add(bl);
+		this.billingId.add(bl.getBillingID());
+		this.time_stamp.add(timestamp);
+		this.fee.add(fee);
+	}
+}
---
+++ src/test/java/edu/rosehulman/billing/datamodels/BillingHistoryTest.java
@@ -0,0 +1,8 @@
+package edu.rosehulman.billing.datamodels;
+import static org.junit.Assert.assertEquals;
+
+public class BillingHistoryTest {
+	
+
+
+}
