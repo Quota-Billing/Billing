350d4abaf0495bafe7ed143909018a75d89658ee
unknown <dingy2@rose-hulman.edu>
2017-10-26 20:58:11 -0400

    Finish test

--- src/test/java/edu/rosehulman/billing/datamodels/BillingHistoryTest.java
+++ src/test/java/edu/rosehulman/billing/datamodels/BillingHistoryTest.java
@@ -1,8 +1,11 @@
 package edu.rosehulman.billing.datamodels;
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
+import edu.rosehulman.billing.models.Billing;
 import edu.rosehulman.billing.models.BillingHistory;
 
 public class BillingHistoryTest {
@@ -33,5 +36,55 @@ public class BillingHistoryTest {
 		BillingHistory bh = new BillingHistory(1);
 		bh.setFee(1.23);
 		assertEquals(1.23, bh.getFee(), 0.0001);
+	}
+	
+	@Test
+	public void TestAddBilling() {
+		BillingHistory bh = new BillingHistory(1);
+		Billing billing = new Billing(1, 1, "planA");
+		bh.addBilling(billing);
+		assertEquals(billing, bh.getBillingList().get(0));
+	}
+	
+	@Test
+	public void TestDeleteBilling() {
+		BillingHistory bh = new BillingHistory(1);
+		Billing billing = new Billing(1, 1, "planA");
+		bh.addBilling(billing);
+		bh.deleteBilling(billing);
+		ArrayList<Billing> ans = new ArrayList<Billing>();
+		assertEquals(bh.getBillingList(), ans);
+		bh.addBilling(billing);
+		bh.deleteBilling(billing.getBillingID());
+		assertEquals(bh.getBillingList(), ans);
+	}
+	
+	@Test
+	public void TestFindBilling() {
+		BillingHistory bh = new BillingHistory(1);
+		Billing billing = new Billing(1, 1, "planA");
+		bh.addBilling(billing);
+		assertEquals(true, bh.findBilling(billing));
+		assertEquals(true, bh.findBilling(billing.getBillingID()));
+		Billing billing2 = new Billing(2, 1, "planA");
+		assertEquals(false, bh.findBilling(billing2));
+		assertEquals(false, bh.findBilling(billing2.getBillingID()));
+	}
+	
+	@Test
+	public void TestUpdateBilling() {
+		BillingHistory bh = new BillingHistory(1);
+		Billing billing = new Billing(1, 1, "planA");
+		bh.addBilling(billing);
+		assertEquals(billing, bh.getBillingList().get(0));
+		//Do it later
+	}
+	
+	@Test
+	public void TestGetBillingId() {
+		BillingHistory bh = new BillingHistory(1);
+		Billing billing = new Billing(1, 1, "planA");
+		bh.addBilling(billing);
+		assertEquals("[1]", bh.getBillingIdList().toString());
 	}
 }
