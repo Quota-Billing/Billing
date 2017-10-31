933a370da03eeffc535c9d63f3c50a7845ac26ef
Fangyuan Wang, 王方圆 <wangfangyuan1995@gmail.com>
2017-10-31 19:06:33 -0400

    update route for quotaReached

--- src/main/java/edu/rosehulman/billing/router/Routes.java
+++ src/main/java/edu/rosehulman/billing/router/Routes.java
@@ -2,7 +2,7 @@ package edu.rosehulman.billing.router;
 
 public class Routes {
 	public static String ADD_USER = "partner/:partnerId/product/:productId/addUser/:userId";
-	public static String QUOTA_REACHED = "/partner/:partnerId/product/:productId/user/:userId/quotaReached/:quotaId/";
+	public static String QUOTA_REACHED = "/partner/:partnerId/produc/:productId/user/:userId/quotaReached/:quotaId/";
 	public static String QUOTA_ADDR = "http://localhost:8082/";
 	//public static String QUOTA_ADDR_notify = "http://localhost:8082/";
 	public static String BILLING_BASE = "/"; // TODO put this in a config file
