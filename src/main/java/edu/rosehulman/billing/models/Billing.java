9ba51673450e84df28a3a670caf05f768e33b3fe
unknown <dingy2@rose-hulman.edu>
2017-11-01 14:43:55 -0400

    Change data model

--- src/main/java/edu/rosehulman/billing/models/Billing.java
+++ src/main/java/edu/rosehulman/billing/models/Billing.java
@@ -1,9 +1,20 @@
 package edu.rosehulman.billing.models;
 
+import org.bson.types.ObjectId;
+import org.mongodb.morphia.annotations.Entity;
+import org.mongodb.morphia.annotations.Id;
+import org.mongodb.morphia.annotations.Property;
+import org.mongodb.morphia.annotations.Reference;
+
+@Entity("Billing")
 public class Billing {
-	
+	@Id
+	private ObjectId id;
+	@Property
 	private int billingID;
+	@Reference
 	private int userID;
+	@Property
 	private String plan;
 	
 	public Billing(int bid, int uid, String plan){
@@ -18,5 +29,21 @@ public class Billing {
 	
 	public int getBillingID(){
 		return this.billingID;
+	}
+	
+	public void setBillingID(int id) {
+		this.billingID = id;
+	}
+	
+	public void setUserID(int id) {
+		this.userID = id;
+	}
+	
+	public void setPlan(String plan) {
+		this.plan = plan;
+	}
+	
+	public String toString() {
+		return "BillingId: " + this.billingID + "\n UserId: " + this.userID + "\n Plan: "+ this.plan +"\n";
 	}
 }
--- src/main/java/edu/rosehulman/billing/models/BillingHistory.java
+++ src/main/java/edu/rosehulman/billing/models/BillingHistory.java
@@ -4,24 +4,45 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Set;
 
-public class BillingHistory {
+import org.bson.types.ObjectId;
+import org.mongodb.morphia.annotations.Entity;
+import org.mongodb.morphia.annotations.Id;
+import org.mongodb.morphia.annotations.Property;
+import org.mongodb.morphia.annotations.Reference;
 
+@Entity("BillingHistory")
+public class BillingHistory {
+	@Id
+	private ObjectId id;
+	@Property
 	private int billinghistoryId;
+	@Reference
 	private HashMap<Integer, Billing> billing;
+	@Property
 	private String time_stamp;
+	@Property
 	private Double fee;
 
 	public BillingHistory(int id) {
+		this.id = new ObjectId();
 		this.billinghistoryId = id;
 		billing = new HashMap<Integer, Billing>();
 		time_stamp = "";
 		fee = 0.0;
 	}
+	
+	public BillingHistory(int id, String time_stamp, double fee) {
+		this.id = new ObjectId();
+		this.billinghistoryId = id;
+		billing = new HashMap<Integer, Billing>();
+		this.time_stamp = time_stamp;
+		this.fee = fee;
+	}
 
 	public void setBillingHistoryId(int id) {
 		this.billinghistoryId = id;
 	}
-	
+
 	public void setFee(double fee) {
 		this.fee = fee;
 	}
@@ -78,6 +99,14 @@ public class BillingHistory {
 
 	public Set<Integer> getBillingIdList() {
 		return this.billing.keySet();
+	}
+
+	public String toString() {
+		String st = "Billing History: " + this.billinghistoryId + "\n";
+		for (int i : this.billing.keySet()) {
+			st += "Billing: " + i + " " + this.billing.get(i) + "\n";
+		}
+		return st;
 	}
 
 }
