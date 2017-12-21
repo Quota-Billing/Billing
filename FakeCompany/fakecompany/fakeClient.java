fe385e5fa038cf5605fd1929f0ae2260fb802b85
Xingcheng <xingcheng@xingchesmacbook.wlan.rose-hulman.edu>
2017-12-21 12:16:45 -0500

    add user from client with real user info

--- FakeCompany/fakecompany/fakeClient.java
+++ FakeCompany/fakecompany/fakeClient.java
@@ -1,5 +1,7 @@
 package fakecompany;
 
+import static spark.Spark.post;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,14 +9,22 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Date;
+import java.io.DataOutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import javax.net.ssl.*;
+
+import javax.net.ssl.HttpsURLConnection;
 
 import javax.swing.JOptionPane;
 
 public class fakeClient {
 	
+	private final String USER_AGENT = "Mozilla/5.0";
 	static int pathmode = 1;
-
-	public static void main(String[] args)throws IOException {
+	static String userName;
+	static String password;
+	public static void main(String[] args)throws Exception {
 		System.out.println("Socket:");
 		BufferedReader socketNumberReader = new BufferedReader(new InputStreamReader(System.in));
 		String socketNumberString = null;
@@ -25,18 +35,63 @@ public class fakeClient {
 		}
 		int socketNumber = Integer.parseInt(socketNumberString);
 
-		String userName = JOptionPane
-				.showInputDialog("Enter your username\n" + "running the date service on port 9090:");
-
-			String serverAddress = JOptionPane.showInputDialog(
-					"Enter IP Address of a machine that is\n" + "running the date service on port 9090:");
+		userName = JOptionPane
+				.showInputDialog("Enter your username\n" + "running the date service on port 8080:");
+		password = JOptionPane
+				.showInputDialog("Enter your password\n" + "running the date service on port 8080:");
+		String serverAddress = JOptionPane.showInputDialog(
+					"Enter IP Address of a machine that is\n" + "running the date service on port 8080:");
 			Socket s = new Socket(serverAddress, socketNumber);
+			
 			System.out.println(new Date().toString());
+			fakeClient http = new fakeClient();
+			System.out.println("\nTesting 2 - Send Http POST request");
+			http.sendPost();
 			try {
 				s.close();
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+	}
+	
+	private void sendPost() throws Exception {
+
+		String url = "http://localhost:8080/user/1155/name/"+userName+"/password/"+password+"/token/dsha2jdaskdhakwda";
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+		//add reuqest header
+		con.setRequestMethod("POST");
+		con.setRequestProperty("User-Agent", USER_AGENT);
+		con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+		String urlParameters = "sn=C02G8416DRJM&cn=&locale=&caller=&num=12345";
+
+		// Send post request
+		con.setDoOutput(true);
+		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+		wr.writeBytes(urlParameters);
+		wr.flush();
+		wr.close();
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'POST' request to URL : " + url);
+		System.out.println("Post parameters : " + urlParameters);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		//print result
+		System.out.println(response.toString());
+
 	}
 }
