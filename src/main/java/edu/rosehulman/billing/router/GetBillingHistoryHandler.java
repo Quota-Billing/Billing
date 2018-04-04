532dd337cc880c2ba00113d0110391ffdb133239
yuzong <yuzong96@gmail.com>
2018-04-04 00:59:35 -0400

    update handlers

--- src/main/java/edu/rosehulman/billing/router/GetBillingHistoryHandler.java
+++ src/main/java/edu/rosehulman/billing/router/GetBillingHistoryHandler.java
@@ -1,16 +1,27 @@
 package edu.rosehulman.billing.router;
 
 import edu.rosehulman.billing.Database;
+import edu.rosehulman.billing.datastore.BillingHistoryDatastore;
 import spark.Request;
 import spark.Response;
 import spark.Route;
 
 public class GetBillingHistoryHandler implements Route{
 	
+	private BillingHistoryDatastore historystore;
+	
+	public GetBillingHistoryHandler(){
+		
+	}
+	
+	public GetBillingHistoryHandler(BillingHistoryDatastore store){
+		this.historystore = store;
+	}
+	
 	public Object handle(Request request, Response response) throws Exception {
 		// TODO Auto-generated method stub
-		String timestamp = request.params(":timestamp");
-		return Database.getInstance().getBillinghistory(timestamp);
+		String id = request.params(":id");
+		return historystore.getBillingHistory(id);
 	}
 
 
